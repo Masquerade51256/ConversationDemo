00019b7898c2bbf74a39f41f4c3f7ebe37f3789b
lyx <40358376+LyXuAnn@users.noreply.github.com>
2019-04-08 12:58:23 +0800

    add similarity

--- Jieba_demo/main.py
+++ Jieba_demo/main.py
@@ -1,6 +1,7 @@
 from makeIDF import IDF
 from match import Match
 
+"""
 d = IDF()
 d.makeIDF()
 m = Match(False)
@@ -9,3 +10,19 @@ while(s!="再见"):
     m.match(s)
     m.makeVec(s)
     s=input()
+"""
+
+d = IDF()
+d.makeIDF()
+m = Match(False)
+s = input()
+s1 = input()
+while(s!= "再见" ):
+    #m.match(s)
+    vec = m.makeVec(s)
+    #m.match(s1)
+    vec1 = m.makeVec(s1)
+    m.similarity(vec, vec1)
+    s = input()
+    s1 = input()
+
--- Jieba_demo/match.py
+++ Jieba_demo/match.py
@@ -1,6 +1,8 @@
 import jieba
 import jieba.analyse
 from utility import Utility as Ut
+import math
+import numpy
 
 class Match:
     def __init__(self, exIDF:bool=True, exStopWords:bool=True, exDict:bool=True):
@@ -27,4 +29,31 @@ class Match:
                 if(x==word):
                     v=w
             vec.append(v)
-        print(vec)
+
+        #print(vec)
+        return vec
+
+    def similarity(self, s1_cut_code, s2_cut_code):
+
+        s1_cut_code = numpy.array(s1_cut_code)
+        s2_cut_code = numpy.array(s2_cut_code)
+        result = s1_cut_code.dot(s2_cut_code) / (
+                    numpy.sqrt(s1_cut_code.dot(s1_cut_code)) * numpy.sqrt(s2_cut_code.dot(s2_cut_code)))
+        print(result)
+
+        '''
+        sum = 0
+        sq1 = 0
+        sq2 = 0
+        for i in range(len(s1_cut_code)):
+            sum += s1_cut_code[i] * s2_cut_code[i]
+            sq1 += pow(s1_cut_code[i], 2)
+            sq2 += pow(s2_cut_code[i], 2)
+
+        try:
+            result = round(float(sum) / (math.sqrt(sq1) * math.sqrt(sq2)), 2)
+        except ZeroDivisionError:
+            result = 0.0
+        print(result)
+        '''
+
